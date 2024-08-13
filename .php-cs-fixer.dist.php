e8675516563a3d6be7cb155516f6d42c098d3835
Anatoliy Melnikov <5785276@gmail.com>
2024-08-13 23:25:56 +0300

    Add PHP CS Fixer rule for adding of license header.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,6 +10,16 @@ $rules = [
     'concat_space' => [
         'spacing' => 'one',
     ],
+    'header_comment' => [
+        'header' => <<<'EOF'
+            This file is part of the TODO Registrar project.
+
+            (c) Anatoliy Melnikov <5785276@gmail.com>
+
+            This source file is subject to the MIT license that is bundled
+            with this source code in the file LICENSE.
+            EOF,
+    ],
     'phpdoc_align' => ['align' => 'left'],
 ];
 
