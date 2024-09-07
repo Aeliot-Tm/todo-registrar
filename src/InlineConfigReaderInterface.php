fc7a834a4f8e143333e6b542689bfdca5a48f361
Anatoliy Melnikov <5785276@gmail.com>
2024-09-07 13:49:57 +0300

    Add license header into php-files

--- src/InlineConfigReaderInterface.php
+++ src/InlineConfigReaderInterface.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TODO Registrar project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Aeliot\TodoRegistrar;
 
 interface InlineConfigReaderInterface
