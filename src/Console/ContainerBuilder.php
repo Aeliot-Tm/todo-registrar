8ab028f7608f24ffbf3d4005ddcde45f1a6fbdaa
Anatoliy Melnikov <5785276@gmail.com>
2025-12-19 20:08:04 +0300

    Fix code style

--- src/Console/ContainerBuilder.php
+++ src/Console/ContainerBuilder.php
@@ -34,4 +34,3 @@ final class ContainerBuilder
         return $container;
     }
 }
-
--- src/Service/Config/ConfigFileDetector.php
+++ src/Service/Config/ConfigFileDetector.php
@@ -1,5 +1,15 @@
 <?php
+
 declare(strict_types=1);
+
+/*
+ * This file is part of the TODO Registrar project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Aeliot\TodoRegistrar\Service\Config;
 
