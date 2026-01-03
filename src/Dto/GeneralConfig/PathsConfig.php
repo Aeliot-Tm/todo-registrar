f0db8e8829e4fb11f0b4063f3f739bb4eeac22d2
Anatoliy Melnikov <5785276@gmail.com>
2026-01-03 15:55:08 +0300

    Move exception general config DTOs to correct namespace

--- src/Dto/GeneralConfig/PathsConfig.php
+++ src/Dto/GeneralConfig/PathsConfig.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace Aeliot\TodoRegistrar\Service\Config;
+namespace Aeliot\TodoRegistrar\Dto\GeneralConfig;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
