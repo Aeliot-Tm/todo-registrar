26ad633a15c55a9adfc16c1a0753f54ed859bace
Anatoliy Melnikov <5785276@gmail.com>
2026-03-01 17:04:51 +0300

    TMP: run test

---
+++ test-todo.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TODO Registrar project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// TODO: Test workflow configuration with YAML config passed via stdin
+// This is a test TODO to verify that the new workflow configuration works correctly.
+// After successful verification, this file can be removed.
+
+echo "Test file for TODO Registrar workflow\n";
